4d64e6b35a61b966f46e36f06050c10777b7f51e
Jordan Brown <jordanfbrown@gmail.com>
2018-01-16 16:09:41 -0800

    Initial commit for scheduling exercise

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import { employees } from './data';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Schedule</h1>
+        <p>Fill me in!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,91 @@
+const employees = [
+  {
+    name: 'Jordan Brown',
+    shifts: [
+      {
+        start_at: '2017-12-18T12:00:00-08:00',
+        end_at: '2017-12-18T17:00:00-08:00',
+        role: 'Server',
+        color: 'red'
+      },
+      {
+        start_at: '2017-12-19T09:00:00-08:00',
+        end_at: '2017-12-19T12:00:00-08:00',
+        role: 'Host',
+        color: 'green'
+      },
+      {
+        start_at: '2017-12-20T09:00:00-08:00',
+        end_at: '2017-12-20T16:00:00-08:00',
+        role: 'Server',
+        color: 'red'
+      },
+      {
+        start_at: '2017-12-21T09:00:00-08:00',
+        end_at: '2017-12-20T14:00:00-08:00',
+        role: 'Host',
+        color: 'green'
+      }
+    ]
+  },
+  {
+    name: 'Tim Cannady',
+    shifts: [
+      {
+        start_at: '2017-12-18T11:00:00-08:00',
+        end_at: '2017-12-18T18:00:00-08:00',
+        role: 'Chef',
+        color: 'orange'
+      },
+      {
+        start_at: '2017-12-19T09:00:00-08:00',
+        end_at: '2017-12-19T15:00:00-08:00',
+        role: 'Dishwasher',
+        color: 'purple'
+      },
+      {
+        start_at: '2017-12-20T09:00:00-08:00',
+        end_at: '2017-12-20T13:00:00-08:00',
+        role: 'Chef',
+        color: 'orange'
+      },
+      {
+        start_at: '2017-12-21T09:00:00-08:00',
+        end_at: '2017-12-21T16:00:00-08:00',
+        role: 'Dishwasher',
+        color: 'purple'
+      }
+    ]
+  },
+  {
+    name: 'Jeff Auston',
+    shifts: [
+      {
+        start_at: '2017-12-18T11:00:00-08:00',
+        end_at: '2017-12-18T18:00:00-08:00',
+        role: 'Chef',
+        color: 'orange'
+      },
+      {
+        start_at: '2017-12-19T09:00:00-08:00',
+        end_at: '2017-12-19T15:00:00-08:00',
+        role: 'Dishwasher',
+        color: 'purple'
+      },
+      {
+        start_at: '2017-12-20T09:00:00-08:00',
+        end_at: '2017-12-20T13:00:00-08:00',
+        role: 'Chef',
+        color: 'orange'
+      },
+      {
+        start_at: '2017-12-21T09:00:00-08:00',
+        end_at: '2017-12-21T16:00:00-08:00',
+        role: 'Dishwasher',
+        color: 'purple'
+      }
+    ]
+  }
+];
+
+export { employees };
